fa0d46414fe68ed3d33b3865ec89aa88fe96c712
DimitrisPappas <pappasdimitrisece@gmail.com>
2025-07-04 01:54:02 +0300

    feat: add input service

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,10 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("App initialization");
+        InputService inputService = new InputService();
+        System.out.println(inputService.getNumbersSequence());
     }
 }
