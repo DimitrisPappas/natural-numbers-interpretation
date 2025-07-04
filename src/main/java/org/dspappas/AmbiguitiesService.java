67faf189c6236920b796f07bf62355e14d99a7bd
Dimitris Pappas <41347409+DimitrisPappas@users.noreply.github.com>
2025-07-04 13:35:43 +0300

    Revert "feat: add two digits variations"
    This reverts commit 7ef8937e633d177b95d1060ce1d3deb80b2d9376.

--- src/main/java/org/dspappas/AmbiguitiesService.java
+++ src/main/java/org/dspappas/AmbiguitiesService.java
@@ -1,7 +1,5 @@
 package org.dspappas;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class AmbiguitiesService {
@@ -28,80 +26,6 @@ public class AmbiguitiesService {
     }
 
     private List<String> generateInterpretations(List<String> numbers) {
-        List<String> results = new ArrayList<>();
-        recursiveVariations(numbers, results, 0, "");
-        return results;
-    }
 
-    private void recursiveVariations(
-            List<String> numbers,
-            List<String> results,
-            int index,
-            String currentVariation
-    ) {
-        if (index >= numbers.size()) {
-            results.add(currentVariation);
-            return;
-        }
-
-        String number = numbers.get(index);
-        List<String> variations = getVariations(number, numbers, index);
-
-        for (String variation : variations) {
-            String nextVariation = currentVariation + variation;
-            recursiveVariations(numbers, results, index+1, nextVariation);
-        }
-    }
-
-    private List<String> getVariations(
-            String originalNumber,
-            List<String> numbers,
-            int index
-    ) {
-        List<String> variations = new ArrayList<>();
-        variations.add(originalNumber);
-
-        if (originalNumber.length() == 2) {
-            variations.addAll(getTwoDigitVariations(originalNumber, numbers, index));
-        } else if (originalNumber.length() == 3) {
-            variations.addAll(getThreeDigitVariations(originalNumber, numbers, index));
-        }
-        return variations;
-    }
-
-    private List<String> getTwoDigitVariations(
-            String originalNumber,
-            List<String> numbers,
-            int index
-    ) {
-        List<String> twoDigitVariations = new ArrayList<>();
-        if (!originalNumber.equals("00")) {
-            if (originalNumber.endsWith("0")) {
-                if (index + 1 < numbers.size()) {
-                    String nextNumber = numbers.get(index+1);
-                    if (nextNumber.length() == 1) {
-                        // Drop 0
-                        String dropZero = originalNumber.substring(0,1);
-                        twoDigitVariations.add(dropZero);
-                    }
-                }
-            } else if (!originalNumber.startsWith("0")) {
-                // Insert 0
-                String firstDigit = originalNumber.substring(0, 1);
-                String secondDigit = originalNumber.substring(1, 2);
-                String digits = firstDigit + "0" + secondDigit;
-                twoDigitVariations.add(digits);
-            }
-        }
-
-        return twoDigitVariations;
-    }
-
-    private List<String> getThreeDigitVariations(
-            String originalNumber,
-            List<String> numbers,
-            int index
-    ) {
-        return Collections.emptyList();
     }
 }
